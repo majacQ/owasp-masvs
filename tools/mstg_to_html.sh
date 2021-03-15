90cf0d77778550053df6682fce5083de63eebd7b
cpholguera <perezholguera@gmail.com>
2021-03-15 12:24:56 +0100

    fix html dir location

--- tools/mstg_to_html.sh
+++ tools/mstg_to_html.sh
@@ -3,5 +3,5 @@ for filename in owasp-mstg/Document/0x05*.md owasp-mstg/Document/0x06*.md; do
     docker run --rm -u `id -u`:`id -g` -v `pwd`:/pandoc dalibo/pandocker --section-divs -f markdown -t html $filename -o $(basename $filename .md).html
 done
 
-mkdir -p html
-mv *.html html
+mkdir -p tools/html
+mv *.html tools/html
