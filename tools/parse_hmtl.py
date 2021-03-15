b0cbb7d1e5012c1cc7f0b0932ed407c2f9aef706
cpholguera <perezholguera@gmail.com>
2021-03-15 11:42:33 +0100

    try mstg convert to html and parse

---
+++ tools/parse_hmtl.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+
+from bs4 import BeautifulSoup
+import re
+import yaml
+from dataclasses import dataclass, asdict
+from pathlib import Path
+
+# docker run --rm -u `id -u`:`id -g` -v `pwd`:/pandoc dalibo/pandocker --section-divs 0x05g-Testing-Network-Communication.md -o 0x05g-Testing-Network-Communication.html
+
+# https://python-markdown.github.io/extensions/fenced_code_blocks/
+# http://zetcode.com/python/beautifulsoup/
+
+MASVS_TITLES = {
+    'V1': 'Architecture, Design and Threat Modeling Requirements',
+    'V2': 'Data Storage and Privacy Requirements',
+    'V3': 'Cryptography Requirements',
+    'V4': 'Authentication and Session Management Requirements',
+    'V5': 'Network Communication Requirements',
+    'V6': 'Platform Interaction Requirements',
+    'V7': 'Code Quality and Build Setting Requirements',
+    'V8': 'Resilience Requirements',
+}
+
+@dataclass
+class TestCase():
+    id: str
+    link: str
+    title: str
+    mstg_ids: list
+    tools: list
+    overview: str
+
+def get_testcases_info(chapter_file_name, chapter):
+    testcases = chapter.find_all(['section'], {"class": "level2"})
+    testcases_info = []
+    
+    for testcase in testcases:
+        title = testcase.find('h2').get_text()
+        id = testcase.get('id')
+        link = f"https://github.com/OWASP/owasp-mstg/blob/master/Document/{chapter_file_name}#{id}"
+        mstg_ids = re.findall(r"MSTG-\w+-\d+", title)
+        tools = get_all_links_to_tools(testcase)
+        overview = get_section_plain_text(testcase, 'overview')
+
+        testcases_info.append(TestCase(id, link, title, mstg_ids, tools, overview))
+
+        # testcases_info.append({
+        #     'id': id,
+        #     'link': link,
+        #     'title': title,
+        #     'mstg-ids': mstg_ids,
+        #     'tools': tools,
+        #     'overview': overview,
+        #     # 'static': get_section_plain_text(testcase, 'static'),
+        #     # 'dynamic': get_section_plain_text(testcase, 'dynamic')
+        # })
+    testcases_info = [asdict(tc) for tc in testcases_info]
+    return testcases_info
+
+def print_yaml(data_dict):
+    print(yaml.dump(data_dict, indent=4, default_flow_style=False, sort_keys=False))
+
+def write_yaml_file(name, data_dict):
+    Path(name).write_text(yaml.dump(data_dict, indent=4, default_flow_style=False, sort_keys=False))
+
+def get_section_plain_text(chapter, starts_with_str):
+
+    sections = chapter.find_all(['section'], {"class": "level3", "id": re.compile(f"{starts_with_str}-?.*")})
+    if sections:
+        return sections[0].get_text()
+    else:
+        return ""
+
+def get_sections_plain_text(chapter, starts_with_str):
+
+    testcase_sections = chapter.find_all(['section'], {"class": "level3", "id": re.compile(f"{starts_with_str}-?.*")})
+
+    for section in testcase_sections:
+        print(section.get_text())
+
+def get_sections_innerHtml(chapter, starts_with_str, remove_title=False):
+    testcase_sections = chapter.find_all(['section'], {"class": "level3", "id": re.compile(f"{starts_with_str}-?.*")})
+
+    for section in testcase_sections:
+        if remove_title:
+            section.contents = section.contents[2:]
+        print(section.encode_contents())
+        
+
+def get_links_to_other_chapters(chapter):
+    links = chapter.find_all(['a'], {"href": re.compile("0x0.*\.md#.*")})
+    return [link.get('href') for link in links]
+
+def get_all_links_to_tools(chapter):
+    links = chapter.find_all(['a'], {"href": re.compile("0x08.*\.md#.*")})
+    return [link.get('href') for link in links]
+
+def get_links_to_tools_per_section(chapter):
+    sections_level2 = chapter.find_all(['section'], {"class": "level2"})
+    tool_links = {}
+    for section in sections_level2:
+        found_links = get_all_links_to_tools(section)
+        tool_links[section.get('id')] = found_links
+    
+    return tool_links
+
+# enhanced_masvs_dict = {}
+# for file in Path('masvs_yaml').glob('*.yaml'):
+#     masvs_dict = yaml.load(open(file))
+#     enhanced_masvs_dict[MASVS_TITLES[file.stem]] = masvs_dict
+
+masvs = yaml.load(open('masvs.yaml'))
+# files = ['Document/0x05b-Basic-Security_Testing.html', 'Document/0x05g-Testing-Network-Communication.html']
+
+# with open(files[1], 'r') as f:
+testcases_info = []
+
+for file in Path('html').glob('*.html'):
+
+    contents = file.read_text()
+
+    chapter = BeautifulSoup(contents, 'lxml')
+
+    # print(get_links_to_other_chapters(chapter))
+
+    # print(get_all_links_to_tools(chapter))
+    # print(get_links_to_tools_per_section(chapter))
+
+    testcases_info += get_testcases_info(f"{file.stem}.md", chapter)
+    # print_yaml(testcases_info)
+
+    # print(get_sections_plain_text(chapter, "overview"))
+    # print(get_sections_innerHtml(chapter, "overview"))
+
+for tc in testcases_info:
+    for id in tc['mstg_ids']:
+        if masvs.get(id): 
+            masvs_req = masvs[id]
+            if not masvs_req.get('links'):
+                masvs_req['links'] = []
+            masvs_req['links'].append(tc['link'])
+        # masvs_dict[id]['solution'].append(tc['overview']) # todo
+
+# print_yaml(masvs)
+write_yaml_file('masvs_full.yaml', masvs)
---
+++ tools/yaml_to_excel.py
@@ -0,0 +1,59 @@
+import yaml
+from openpyxl import load_workbook
+
+MASVS_TITLES = {
+    'V1': 'Architecture, Design and Threat Modeling Requirements',
+    'V2': 'Data Storage and Privacy Requirements',
+    'V3': 'Cryptography Requirements',
+    'V4': 'Authentication and Session Management Requirements',
+    'V5': 'Network Communication Requirements',
+    'V6': 'Platform Interaction Requirements',
+    'V7': 'Code Quality and Build Setting Requirements',
+    'V8': 'Resilience Requirements',
+}
+
+def get_hyperlink(url):
+    title = url.split('#')[1].replace('-',' ').capitalize() 
+    return f'=HYPERLINK(CONCATENATE(BASE_URL,"{url}"), "{title}")'
+
+CHECKMARK = "✓"
+
+masvs_dict = yaml.load(open('masvs_full.yaml'))
+
+wb = load_workbook(filename='checklist.xlsx')
+table = wb['Security Requirements - Android']
+
+row=4
+col_id=2
+col_mstg_id=3
+col_text=4
+col_l1=5
+col_l2=6
+col_link=8
+
+for mstg_id, req in masvs_dict.items():
+    req_id = req['id'].split('.') 
+    category = req_id[0]
+    subindex = req_id[1]
+
+    if subindex == '1':
+        category_id = f"V{category}"
+        category_title = MASVS_TITLES[category_id]
+        table.cell(row=row,column=col_id).value = category_id
+        table.cell(row=row,column=col_id+2).value = category_title
+        row = row+1
+    
+    l1 = CHECKMARK if req['L1'] else ""
+    l2 = CHECKMARK if req['L2'] else ""
+    r = CHECKMARK if req['R'] else ""
+
+    table.cell(row=row,column=col_id).value = req['id']
+    table.cell(row=row,column=col_mstg_id).value = mstg_id
+    table.cell(row=row,column=col_text).value = req['text']
+    table.cell(row=row,column=col_l1).value = l1
+    table.cell(row=row,column=col_l2).value = l2
+    if req.get('links'):
+        table.cell(row=row,column=col_link).value = get_hyperlink(req['links'][0])
+    row = row+1
+
+wb.save(filename='checklist_out.xlsx')
