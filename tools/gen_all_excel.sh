c3a006870e5031ec6d828164d89eb3b0a892f0f9
cpholguera <perezholguera@gmail.com>
2021-03-16 19:22:17 +0100

    try matrix

--- tools/gen_all_excel.sh
+++ tools/gen_all_excel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LanguageArray=("de" "en" "es" "fa" "fr" "hi" "ja" "ko" "ru" "zhcn" "zhtw")
 for lang in ${LanguageArray[*]}; do
-    python3 export.py -f yaml -l $lang > masvs_$lang.yaml
+    # python3 export.py -f yaml -l $lang > masvs_$lang.yaml
     python3 parse_html.py -m masvs_$lang.yaml -i html -o masvs_full_$lang.yaml
     python3 yaml_to_excel.py -m masvs_full_$lang.yaml -i checklist.xlsx -o checklist_$lang.xlsx
 done
